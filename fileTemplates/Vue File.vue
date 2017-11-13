be9a6b60bbb5f79e21e14fc560d572b5ead2d8fb
janekkkk <janekozga1@gmail.com>
2017-11-13 17:53:28 +0100

    PS-2016.3.2 <janek@Janek-PC Create baseRefactoring.xml, _@user_Material Theme - Default.icls, markdown.xml, IntelliLang.xml, Unnamed.php, ide.general.xml, git.xml, project.default.xml, diff.xml, PHP Constructor.php, other.xml, file.template.settings.xml, ignore.xml, find.xml, keymap.xml, code.style.schemes.xml, watcherDefaultTasks.xml, colors.scheme.xml, Vue File.vue, databaseDrivers.xml, Default.xml, laf.xml, path.macros.xml, phonegap.xml, Default _1_.xml, web-browsers.xml, databaseSettings.xml, webServers.xml, debugger.xml

---
+++ fileTemplates/Vue File.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class='msg'>{{myMessage}}</div>
+</template>
+<script>
+export default {
+     data() { 
+         return { 
+             myMessage : 'My message goes here'
+         }
+     },
+        methods: {
+        },
+        async created() {},
+}
+</script>
+<style lang="sass" rel="stylesheet/scss">
+
+</style>
